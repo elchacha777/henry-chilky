ba92d2c03027ea51614f029c56ef981387b48f34
Atai <zver.cm@gmail.com>
2022-10-17 11:07:18 +0600

    asdasdasda

--- db/orm.py
+++ db/orm.py
@@ -1,6 +1,6 @@
 from peewee import *
 
-db_psql = PostgresqlDatabase(database='googlegmail', user='postgres', password='password', host='postgres_host', port=5432)
+db_psql = PostgresqlDatabase(database='postgres', user='postgres', password='password', host='postgres_host', port=5432)
 
 
 class Emails(Model):
