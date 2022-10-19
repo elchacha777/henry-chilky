2ea3e784096755f23b83b84cf04b25172e3f48a2
Atai <zver.cm@gmail.com>
2022-10-19 12:35:35 +0600

    salkjdklalsjdakldlkaljsd

--- utils/button.py
+++ utils/button.py
@@ -17,7 +17,7 @@ def get_button(buttons):
             for button in buttons:
                 review = button.get_attribute('data-value')
                 logger.info(f'get attribute {review}')
-                if review == 'Оставить отзыв':
+                if review == 'Rezension schreiben':
                     time.sleep(1)
                     return button
                 elif review == 'Write a review':
